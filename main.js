6f140e565d293c9cf0120b76cece60543e582e83
Gungod <gungod0601@gmail.com>
2022-02-21 21:29:22 +0900

    express-session studying commit & push again

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var express = require('express');
-
 var topic = require('./lib/topic');
 var bodyparser = require('body-parser');
 var compression = require('compression');
@@ -10,12 +9,32 @@ var helmet = require('helmet');
 const { response } = require('express');
 var usermgmt = require('./routes/usermgmt_route');
 
+var session = require('express-session');
+var app = express();
+var sessionStore = require('./lib/session_db.js');
+
 var app = express();
 // middleware 사용 부분
 app.use(bodyparser.urlencoded({extended: false})); // bodyparser : post 방식 body parsing용 -> express 기본 탑재
 app.use(compression()); // 클라이언트 ~ 서버 전송내용 압축
 app.use(express.static('public')); // public dir 내부 static 파일 찾기. public 내부의 파일 디렉토리를 URL 통해 접근 가능해짐. : 보안성 안전
 app.use(helmet()); // 각종 보안솔루션 제공 모듈!!
+
+app.use(session({
+  key: 'is_logined',
+  secret: 'mysecret',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(session({
+  key: 'nickname',
+  secret: 'mysecret',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+}));
 
 // get방식으로 들어오는 요청에만 전부 응답하고, 타 방식에는 db 가져오지 않음.
 app.get('*',function(request, response, next){
@@ -39,8 +58,6 @@ app.use('/topic', topicRouter);
 
 app.use('/author', authorRouter);
 // author router 사용
-
-
 
 // routing : 사용자가 path를 따라 들어올 때 적당한 대답을 해주는 것!
 app.get('/', function(request, response){
